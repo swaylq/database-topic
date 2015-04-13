5f9e2b852ced10512aa2b92ad4848c708ea0434f
swaylq <501527961@qq.com>
2015-04-14 01:33:30 +0800

    更改数据库的连接

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 var mongoose = require( 'mongoose' );
-var db = mongoose.createConnection('localhost','databaseTopic');
+mongoose.connect('mongodb://localhost/databaseTopic');
+var db = mongoose.connection;
 db.on('error',console.error.bind(console,'连接错误:'));
 db.once('open',function(){
     console.log('connected');
