94323589a8011fb15029d9c63110efbb7bc74620
swaylq <501527961@qq.com>
2015-04-14 11:26:21 +0800

    完成了创建订单

---
+++ models/orders.js
@@ -0,0 +1,15 @@
+module.exports = function (mongoose) {
+    var orderSchema = new mongoose.Schema({
+        consignee_name: String,
+        consignee_address: String,
+        price: Number,
+        created_at: Date
+    });
+
+    orderSchema.methods.speak = function () {
+        console.log(this);
+    };
+
+    orderModel = mongoose.model('order', orderSchema);
+    return orderModel;
+}
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('../db.js');
 var bookModel = require('../models/books.js')(mongoose);
+var orderModel = require('../models/orders.js')(mongoose);
+var orderBookModel = require('../models/order_books.js')(mongoose);
 
 router.get('/book/create/exmple', function (req, res, next) {
     var book = new bookModel({
@@ -26,6 +28,30 @@ router.get('/book/list', function (req, res, next) {
     bookModel.find().exec(function (err, books){
         res.send(books);
     })
+});
+
+router.post('/order/create', function (req, res, next){
+    var currentDate = new Date();
+    var totalPrice = 0;
+    req.params.books.forEach(function (book){
+        totalPrice += book.price * book.number;
+    })
+    var order = new orderModel({
+        consignee_name: req.params.consignee_name,
+        consignee_address: req.params.consignee_address,
+        price: totalPrice,
+        created_at: currentDate
+    });
+    order.save();
+    req.params.books.forEach(function (book){
+        var orderBook = new orderBookModel({
+            order_id: order._id,
+            book_id: book._id,
+            number: book.number
+        });
+        orderBook.save();
+    });
+    res.send({'msg': '下单成功'});
 });
 
 router.get('/', function(req, res, next) {
